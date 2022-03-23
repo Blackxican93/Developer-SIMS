24a4410159eb2b64c233b5553f40fe26f2e9f0c1
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-03-23 16:22:58 -0500

    Questions are read from external file, AB#273

--- src/Questions.java
+++ src/Questions.java
@@ -1,2 +1,19 @@
+import java.io.*;
+
+
+
 public class Questions {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(
+                "/Users/jr/Desktop/House of Madness/Questions.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
+    }
+
 }
