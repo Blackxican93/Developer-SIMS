250b761b15d9fa81919d9bfa9e545cc50d5f2ae4
mnievesx <marianievesx@gmail.com>
2022-03-24 14:33:45 -0700

    Game intro added along with option to quit game, AB#110

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Insert game intro");
+        System.out.println("Game intro added here.");
     }
+
 }
 
 
