135ef37238ffb89f93002bd265272a42c8ea5056
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-03-27 18:08:52 -0500

    Players can add in game commands using external file (still need to add user prompt to ask what they want to add), AB#133

---
+++ src/AddGameCommands.java
@@ -0,0 +1,45 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+public class AddGameCommands {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            Object obj = jsonParser.parse(new FileReader("/Users/jr/Desktop/House of Madness/gameCommands.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+
+            System.out.println(jsonArray);
+
+            JSONObject addGameCommands = new JSONObject();
+            addGameCommands.put("commandName", "run");
+            addGameCommands.put("commandDescription", "The monster is on to you so you better run! ");
+            addGameCommands.put("commandExample", "run office");
+
+
+            jsonArray.add(addGameCommands);
+
+            System.out.println(jsonArray);
+
+            FileWriter file = new FileWriter("/Users/jr/Desktop/House of Madness/gameCommands.json");
+            file.write(jsonArray.toJSONString());
+            file.flush();
+            file.close();
+
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
