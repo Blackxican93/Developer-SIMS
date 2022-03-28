02d88a25e0eb758d4e7c1ec60d2d6867d16b223d
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-03-28 17:11:17 -0500

    Players can enter in any location and it will be added to the json file, AB#131

--- src/AddLocations.java
+++ src/AddLocations.java
@@ -14,6 +14,11 @@ import org.json.simple.parser.ParseException;
 
 
 public class AddLocations {
+    static String input = "";
+    static String locationNameOption = "";
+    static String locationDescriptionOption = "";
+    static String locationItemsOption = "";
+    static String locationDirectionsOption = "";
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
@@ -26,24 +31,36 @@ public class AddLocations {
 
             System.out.println(jsonArray);
 
-            JSONObject newLocation = new JSONObject();
+            JSONObject addLocation = new JSONObject();
             Scanner sc = new Scanner(System.in);
             System.out.println("Would you like to add a location to the game? Type Y or N ");
 
-            String inputY = sc.nextLine();
-            System.out.println("You have entered: " + inputY);
+            input = sc.nextLine();
+            System.out.println("You have entered: " + input);
 
-            System.out.println("You have the option of adding in one of two locations. You can add a gym or man cave. Type 1 for the man cave or 2 for the gym. ");
-            String locationNameOption = sc.nextLine();
-            newLocation.put("locationName", "man cave");
+            System.out.println("You have the option of creating a location inside of the house. Simply type in your location name using one word. ");
+            locationNameOption = sc.nextLine();
+            System.out.println("You have entered: " + locationNameOption);
+            addLocation.put("locationName", locationNameOption);
 
-            System.out.println("You have the option of adding in one of two location items. You can add a hookah or beer. Type 1 for the hookah or 2 for the beer. ");
-            String locationItemOption = sc.nextLine();
-            newLocation.put("locationItems", "beer");
+            System.out.println("You have the option of creating a location direction inside of the house. Simply type in your location direction. ");
+            locationDirectionsOption = sc.nextLine();
+            System.out.println("You have entered: " + locationDirectionsOption);
+            addLocation.put("locationDirections", locationDirectionsOption);
+
+            System.out.println("You have the option of creating a location description. Simply type in your location description. ");
+            locationDescriptionOption = sc.nextLine();
+            System.out.println("You have entered: " + locationDescriptionOption);
+            addLocation.put("locationDescription", locationDescriptionOption);
+
+            System.out.println("You have the option of creating a location item. Simply type in your location item. ");
+            locationItemsOption = sc.nextLine();
+            System.out.println("You have entered: " + locationItemsOption);
+            addLocation.put("locationItems", locationItemsOption);
 
 
-            jsonArray.add(newLocation);
-
+            jsonArray.add(addLocation);
+            System.out.println("Your options have been added to the list ");
             System.out.println(jsonArray);
 
             FileWriter file = new FileWriter("/Users/jr/Desktop/House of Madness/location.json");
