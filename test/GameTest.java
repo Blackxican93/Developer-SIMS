69caa18f86719629f9c848181af134b3764a0011
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-04-03 20:37:22 -0500

    Added in test folder

---
+++ test/GameTest.java
@@ -0,0 +1,81 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void takeOb() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void dropOb() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void movePlayerTo() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void goN() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void goS() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void goW() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void goE() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void look() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void showStr() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void showInventory() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void showIntro() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void interviewQuestions() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void runCommand() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void addGameCommands() {
+//        assertEquals("", "");
+    }
+
+    @Test
+    public void addItems() {
+//        assertEquals("", "");
+    }
+}
